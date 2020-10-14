d35be33058f9bc486da206afa88b87cb253695d6
Sakshi725744 <sjparikh78@gmail.com>
2020-10-14 19:22:42 +0530

    added comments to the code

--- C++/Diffie Hellman.cpp
+++ C++/Diffie Hellman.cpp
@@ -1,8 +1,12 @@
+//The Diffie-Hellman algorithm is being used to establish a shared secret
+//that can be used for secret communications while exchanging data over a
+//public network using the elliptic curve to generate points and get the secret key using the parameters
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
-
+//OTP generating system for ATM machine
 int main()
 {
     int n,p;
