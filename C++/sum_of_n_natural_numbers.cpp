d295c08f54e897a25aadc095c09384c4b95dbb34
Siva Kumar G <61716135+SivaKumar2001@users.noreply.github.com>
2020-10-16 20:52:42 +0530

    Update sum_of_n_natural_numbers.cpp

--- C++/sum_of_n_natural_numbers.cpp
+++ C++/sum_of_n_natural_numbers.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main()
 {
     int n;
-    cin >>n;
-    int a = (n*(n+1)/2); // if n is 10 -> (10*(10+1)/2) = 55
-    cout <<a;
-
+    cout<<"Enter the value of n: "; 
+    cin>>n;
+    // if n is 10 -> (10*(10+1)/2) = 55
+    int sum = (n*(n+1)/2); 
+    cout<<sum;
+    return 0;
 }
