8b847ca7f039d48d8c197cceaed7b6b72bcd0003
Badshah <68102965+aman220@users.noreply.github.com>
2021-10-03 11:21:31 +0530

    dictionary.py
    Working of dictionary operator in python

---
+++ Python/dictionary.py
@@ -0,0 +1,9 @@
+d={
+"Aligarh":"It is the city in up ",
+"bharatpur":"it is the city in rajisthan ",
+"delhi ":"it is the capital of india",
+"Mumbai":"it is the city in maharashtra"
+}
+print("enter the name which you want to search ")
+n1=input()
+print(d[n1])
