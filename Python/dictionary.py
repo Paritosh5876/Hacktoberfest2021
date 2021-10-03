1c162531ceea9606e58ada322314882d94f4766e
Prakul Patel <72519744+Paritosh5876@users.noreply.github.com>
2021-10-03 14:51:52 +0530

    Update dictionary.py

--- Python/dictionary.py
+++ Python/dictionary.py
@@ -1,8 +1,8 @@
 d={
-"Aligarh":"It is the city in up ",
-"bharatpur":"it is the city in rajisthan ",
-"delhi ":"it is the capital of india",
-"Mumbai":"it is the city in maharashtra"
+"Aligarh":"It is the city in UP ",
+"bharatpur":"it is the city in Rajasthan",
+"delhi ":"it is the capital of India",
+"Mumbai":"it is the city in Maharashtra"
 }
 print("enter the name which you want to search ")
 n1=input()
