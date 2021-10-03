66edb6dc93e77785f66f4b89fb08afad69084ffa
Rajit <73546886+rajitkuthialamuj@users.noreply.github.com>
2021-10-03 16:42:35 +0530

    DuplicateItem Checker in Java

---
+++ Java/DuplicateArray.java
@@ -0,0 +1,25 @@
+public class DuplicateArray {
+    static boolean DuplicateFun(int[] data)
+    {
+      
+        for(int i = 0;i<data.length;i++)
+        {
+            for(int j = 0;j<data.length;j++)
+            {
+                if(data[i]==data[j]){
+                    return true;
+                }
+                
+            }
+        }
+        return false;
+      
+    }
+  
+    public static void main(String args[])
+    {
+        int arr []=  {9,9,8,2,7};
+       System.out.print( DuplicateFun(arr));
+        
+    }
+} 
