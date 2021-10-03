b4009f3142730030c68bacb44fb17bb272213a73
Shreyash Arya <shreyash15097@iiitd.ac.in>
2021-10-03 16:29:49 +0530

    Update import_any_python_file_example.py
    Bug fixes

--- Python/import_any_python_file_example.py
+++ Python/import_any_python_file_example.py
@@ -2,8 +2,11 @@
 
 import BubbleSort
 
-array_input = input()   # Example: 2 3 4 5
+arr = input()   # Example: 2 3 4 5
 
 array = list(map(int,arr.split(' '))) 
 
-bubble_sort(array)
+print(BubbleSort.bubble_sort(array))
+
+
+
