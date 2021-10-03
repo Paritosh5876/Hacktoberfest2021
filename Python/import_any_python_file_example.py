fcaf007e83d399f46f21bc0f2de7cbac6866cb3f
Shreyash Arya <shreyash15097@iiitd.ac.in>
2021-10-03 15:57:26 +0530

    Create import_any_python_file_example.py

---
+++ Python/import_any_python_file_example.py
@@ -0,0 +1,7 @@
+## Suppose I want to import another python file and it's function simply it with the file name and use the function directly
+
+import BubbleSort
+
+array = [5, 3, 8, 6, 7, 2]
+
+bubble_sort(array)
