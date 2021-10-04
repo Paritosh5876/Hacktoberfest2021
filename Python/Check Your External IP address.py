ee35fdbe7669c5ba5f41dfcd1ec34de2191dead4
pembattech <pemba.t.tech.5@gmail.com>
2021-10-04 07:37:51 +0545

    Making Check Your External IP address compatible to python3

--- Python/Check Your External IP address.py
+++ Python/Check Your External IP address.py
@@ -1,14 +1,10 @@
-import urllib
+import urllib.request
 import re
 
-print "we will try to open this url, in order to get IP Address"
-
+print("we will try to open this url, in order to get IP Address")
 url = "http://checkip.dyndns.org"
+print(url)
+request_ = urllib.request.urlopen(url).read().decode('utf-8')
+theIP = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", request_)
 
-print url
-
-request = urllib.urlopen(url).read()
-
-theIP = re.findall(r"d{1,3}.d{1,3}.d{1,3}.d{1,3}", request)
-
-print "your IP Address is: ",  theIP
+print("your IP Address is: ",  theIP)
