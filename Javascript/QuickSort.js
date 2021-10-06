84b9409ff84d802ada93622e840645bd1014f385
shambhuj071 <92019573+shambhuj071@users.noreply.github.com>
2021-10-06 20:25:17 +0530

    Update QuickSort.js

--- Javascript/QuickSort.js
+++ Javascript/QuickSort.js
@@ -1,5 +1,8 @@
-// Description: Function to perform the Quick Sort algorithm on an array of numbers
-// Expected Output: returns the sorted array
+/*
+QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. 
+The below picks first element as pivot.The key process in quickSort is partition().
+Target of partitions is, given an array and an element x of array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) after x.
+*/
 
 // Helper function used to swap values based on indices given
 const swap = (arr, a, b)=>{
