f3b5b35a4dac1517911c74a5019c15231a86929e
shambhuj071 <92019573+shambhuj071@users.noreply.github.com>
2021-10-06 20:23:36 +0530

    Update QuickSort.rb

--- Ruby/QuickSort.rb
+++ Ruby/QuickSort.rb
@@ -1,4 +1,6 @@
-#Quick Sort Implementation in Ruby
+#QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around the picked pivot. 
+#The below picks first element as pivot.The key process in quickSort is partition().
+#Target of partitions is, given an array and an element x of array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) after x.
 
 #Conquer
 def Partition(array, left, right)
