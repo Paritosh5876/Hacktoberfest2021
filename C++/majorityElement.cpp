1efe3f2f1eb538a10c92aeed38999244c4495ca5
Ayush Yadav <ayushy738@gmail.com>
2021-10-06 20:57:55 +0530

    added description and about code

--- C++/majorityElement.cpp
+++ C++/majorityElement.cpp
@@ -1,3 +1,4 @@
+//link to the problem : https://leetcode.com/problems/majority-element/
 // Given an array nums of size n, return the majority element.
 // The majority element is the element that appears more than ⌊n / 2⌋ times.
 // You may assume that the majority element always exists in the array.
