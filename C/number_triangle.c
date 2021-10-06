57fd3b6e3db6226b5cd0921307e2c442b8886037
Amil Utkarsh Gupta <64407800+Amil-Gupta@users.noreply.github.com>
2021-10-06 14:13:12 +0530

    Added functionality, made code better presented
    The range now goes from m to n rather than 1 to n.
    Added proper indentation to make code look better.

--- C/number_triangle.c
+++ C/number_triangle.c
@@ -1,28 +1,28 @@
-    #include<stdio.h>    
-    #include<stdlib.h>  
-    
-    int main(){  
-      int i,j,k,l,n;    
+#include<stdio.h>    
+#include<stdlib.h>  
+
+int main(){  
+    int i,j,k,l,m,n;    
       
     system("cls");  
-    printf("enter the range=");    
-    scanf("%d",&n);    
+    printf("enter the range : ");    
+    scanf("%d%d",&m,&n);    
     
-    for(i=1;i<=n;i++)    
+    for(i=m;i<=n;i++)    
     {    
-    for(j=1;j<=n-i;j++)    
-    {    
-    printf(" ");    
+        for(j=m-1;j<=n-i;j++)    
+        {    
+            printf(" ");    
+        }    
+        for(k=m;k<=i;k++)    
+        {    
+            printf("%d",k);    
+        }    
+        for(l=i-1;l>=m;l--)    
+        {    
+            printf("%d",l);    
+        }    
+        printf("\n");    
     }    
-    for(k=1;k<=i;k++)    
-    {    
-    printf("%d",k);    
-    }    
-    for(l=i-1;l>=1;l--)    
-    {    
-    printf("%d",l);    
-    }    
-    printf("\n");    
-    }    
-    return 0;  
-    }  
+    return 0;
+}
