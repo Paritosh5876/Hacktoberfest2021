f0e0c8808f6e7ac85b1d5c2eded709559955cdcd
Futureworld1 <92092642+Futureworld1@users.noreply.github.com>
2021-10-07 17:39:29 +0530

    Abstraction Code

---
+++ Java/Abstraction.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+  
+class implementAbstraction
+{
+    private:
+        int a, b;
+  
+    public:
+      
+        void set(int x, int y)
+        {
+            a = x;
+            b = y;
+        }
+          
+        void display()
+        {
+            cout<<"a = " <<a << endl;
+            cout<<"b = " << b << endl;
+        }
+};
+  
+int main() 
+{
+    implementAbstraction obj;
+    obj.set(10, 20);
+    obj.display();
+    return 0;
+}
