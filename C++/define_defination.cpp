6a169e480bb8f00da88ca78df5152d5493183462
Rajit <73546886+rajitkuthialamuj@users.noreply.github.com>
2021-10-08 11:42:29 +0530

    Created #define defination

---
+++ C++/define_defination.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#define pi 3.14
+#define MAX 1000000007
+
+using namespace std; 
+
+int main(){
+    cout<<pi;
+    cout<<endl<<MAX<<" this value is used for preventing overflow";
+
+    return 0;
+}
+
