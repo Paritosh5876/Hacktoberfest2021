83efc41b6e6b92d71849e54a5ec667f27dadd39d
Mohi <mohamad.kanjo@skola.uppsala.se>
2021-10-20 20:09:39 +0200

    Added a second preferable approach to solve this problem

--- Javascript/capitalizeEachWord.js
+++ Javascript/capitalizeEachWord.js
@@ -1,6 +1,10 @@
-const capitalizeTwo = (str) => {
+const capitalize = (str) => {
   const newStr = str.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
   return newStr
+}
+
+const captialize2ndApproach = (str) => {
+  return str.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 }
 
 console.log(capitalize('peter parker')) // Expected output "Peter Parker"
